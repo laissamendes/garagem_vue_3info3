7c2bd191c8a4e99114ff5be52b0cfdb9552cafca
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-04-01 15:08:19 -0300

    Acessório, Cor e Marca adicionados ao vue com todas as obrigatóriedades

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@ import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/acessorios">Acessórios</router-link> |
+    <router-link to="/categorias">Categorias</router-link> |
+    <router-link to="/cores">Cores</router-link> |
+    <router-link to="/marcas">Marcas</router-link>
   </header>
   <main>
     <RouterView />
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
+import CorView from "../views/CorView.vue";
+import MarcaView from "../views/MarcaView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +15,24 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
       path: "/categorias",
       name: "categorias",
       component: CategoriaView,
     },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },{
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
+    }
   ],
 });
 
